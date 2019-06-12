f2a4bfd84096382e0f515647b016533bebf71d5b
Griffin <griffincriste@gmail.com>
2019-06-11 23:02:12 -0500

    added api call for recipe image and title, print image and title to page, also added buttons and input for new ingredient button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,113 @@
-console.log("HI")
 
-$.ajax({
-    url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com&apikey=4dfa7d01a2msh5e0284ccab04185p1fe451jsn99c32d12fa03",
+// var ingredient = "chicken+steak";
+// $.ajax({
+//     url: "https://api.edamam.com/search?q="+ ingredient + "&app_id=b8fd7b5e&app_key=afe2eb090dd94ac02af9b397851a630a",
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response)
+//   });
+
+
+  var ingredient = ["eggs+", "apples+", "rice"];
+  var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=10&ingredients=" + ingredient;
+   
+  $.ajax({
+    url: queryUrl,
+    headers: { 
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+      'X-RapidAPI-Key': '98db7bca28msh8d5ab6517d13d17p17dbecjsn45b21b9d6bda'},
     method: "GET"
   }).then(function(response) {
-    console.log(response);
+    var recipe = response
+    console.log(recipe);
   });
- // 4dfa7d01a2msh5e0284ccab04185p1fe451jsn99c32d12fa03
 
-  //"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com&apikey=4dfa7d01a2msh5e0284ccab04185p1fe451jsn99c32d12fa03"
+  
+
+  var popularIngredients = ["chicken", "steak", "pork", "salt", "cumin", "tomatoes", "eggs", "milk", "rice", "cheese", "chicken broth", "tomato paste", "rolled oats", "nuts", "worcestershire sauce", "flour", "garlic", "onions", "oregano", "cinnamon", "cayenne", "black pepper", "kosher salt", "yogurt", "oil", "vingear", "potatoes", "syrup", "beans", "lemons", "tuna", "butter", "bell peppers", "sugar"]
+  // var popularMeats = [""];
+
+  // var popularVegetables = [""];
+
+  // var popularFruits = [""];
+
+  // var popularDairy = [""];
+
+  // var popularSpices = [""];
+
+function displayIngredient(){
+  $("#ingredientsection").empty();
+
+  for (var i =0; i<popularIngredients.length; i++){
+    var newButton = $("<button>").text(popularIngredients[i]);
+      //gives the button a class of new button
+          newButton.addClass("ingredients");
+          //gives the data attribute of data-name
+          newButton.attr("data-name", popularIngredients[i]);
+          //puts button in button section
+          $("#ingredientsection").append(newButton);
+    }
+  }
+
+
+  $("#add-ingredient-button").on("click", function(event){
+
+    event.preventDefault();
+    var newIngredient = $("#ingredient-input").val().trim();
+    popularIngredients.push(newIngredient)
+    
+    displayIngredient()
+    $("#ingredient-input").val("");
+
+  })
+
+displayIngredient();
+
+ $("#submit-button").on("click", function(){
+
+  displayRecipe()
+
+});
+
+
+function displayRecipe(){
+console.log("it worked")
+// var movie = $(this).attr("data-name");
+
+$.ajax({
+  url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=apples%2Cflour%2Csugar",
+  headers: { 
+    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+    'X-RapidAPI-Key': '98db7bca28msh8d5ab6517d13d17p17dbecjsn45b21b9d6bda'},
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
 
 
 
 
 
-// unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/4632/summary")
-// .header("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
-// .header("X-RapidAPI-Key", "4dfa7d01a2msh5e0284ccab04185p1fe451jsn99c32d12fa03")
-// .end(function (result) {
-//   console.log(result.status, result.headers, result.body);
-// });
+for (var i =0; i<10;i++){
+  var recipeDiv = $("<div>");
+   
+
+  var recipeImage = $("<img>")
+  var recipeTitle = $("<div>")
+
+  recipeImage.attr("src", response[i].image);
+  
+  recipeTitle.text("Title: "+ response[i].title)
+
+  recipeDiv.append(recipeImage, recipeTitle)
+  $("#recipesection").append(recipeDiv);
+
+
+console.log(recipeImage)
+console.log(recipeTitle)
+
+
+  
+}
+})
+
+};
