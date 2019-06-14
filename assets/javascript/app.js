36ebdb491f21da4e7bc740b23622706e27495a42
Griffin <griffincriste@gmail.com>
2019-06-14 11:04:38 -0500

    working spoonacaular api call for recipes based on ingredients

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,100 @@
 
-  var ingredient = ["eggs+", "apples+", "rice"];
-  var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=10&ingredients=" + ingredient;
-  
+var ingredient = ["eggs+", "apples+", "rice+"];
+var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=10&ingredients=" + ingredient + "+";
+var recipeIdArray = [];
+var recipeString;
 
-  var popularIngredients = ["chicken", "steak", "pork", "salt", "cumin", "tomatoes", "eggs", "milk", "rice", "cheese", "chicken broth", "tomato paste", "rolled oats", "nuts", "worcestershire sauce", "flour", "garlic", "onions", "oregano", "cinnamon", "cayenne", "black pepper", "kosher salt", "yogurt", "oil", "vingear", "potatoes", "syrup", "beans", "lemons", "tuna", "butter", "bell peppers", "sugar"]
-  // var popularMeats = [""];
+var popularIngredients = ["chicken", "steak", "pork", "salt", "cumin", "tomatoes", "eggs", "milk", "rice", "cheese", "chicken broth", "tomato paste", "rolled oats", "nuts", "worcestershire sauce", "flour", "garlic", "onions", "oregano", "cinnamon", "cayenne", "black pepper", "kosher salt", "yogurt", "oil", "vingear", "potatoes", "syrup", "beans", "lemons", "tuna", "butter", "bell peppers", "sugar"]
+// var popularMeats = [""];
 
-  // var popularVegetables = [""];
+// var popularVegetables = [""];
 
-  // var popularFruits = [""];
+// var popularFruits = [""];
 
-  // var popularDairy = [""];
+// var popularDairy = [""];
 
-  // var popularSpices = [""];
+// var popularSpices = [""];
 
-function displayIngredient(){
+function displayIngredient() {
   $("#ingredientsection").empty();
 
-  for (var i =0; i<popularIngredients.length; i++){
+  for (var i = 0; i < popularIngredients.length; i++) {
     var newButton = $("<button>").text(popularIngredients[i]);
-        //gives the button a class of new button
-        newButton.addClass("ingredients");
-        //gives the data attribute of data-name
-        newButton.attr("data-name", popularIngredients[i]);
-        //puts button in button section
-        $("#ingredientsection").append(newButton);
-    }
+    //gives the button a class of new button
+    newButton.addClass("ingredients");
+    //gives the data attribute of data-name
+    newButton.attr("data-name", popularIngredients[i]);
+    //puts button in button section
+    $("#ingredientsection").append(newButton);
   }
+}
 
 
-  $("#add-ingredient-button").on("click", function(event){
-    event.preventDefault();
-    var newIngredient = $("#ingredient-input").val().trim();
-    popularIngredients.push(newIngredient)
-    displayIngredient()
-    $("#ingredient-input").val("");
-  })
+$("#add-ingredient-button").on("click", function (event) {
+  event.preventDefault();
+  var newIngredient = $("#ingredient-input").val().trim();
+  popularIngredients.push(newIngredient)
+  displayIngredient()
+  $("#ingredient-input").val("");
+})
 
 
 displayIngredient();
- $("#submit-button").on("click", function(){
+$("#submit-button").on("click", function () {
   displayRecipe()
 });
 
-function displayRecipe(){
-console.log("it worked")
+function displayRecipe() {
+  console.log("it worked")
 
-$.ajax({
-  url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=apples%2Cflour%2Csugar",
-  headers: { 
-    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-    'X-RapidAPI-Key': '98db7bca28msh8d5ab6517d13d17p17dbecjsn45b21b9d6bda'},
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
+  $.ajax({
+    url: queryUrl,
+    headers: {
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+      'X-RapidAPI-Key': '98db7bca28msh8d5ab6517d13d17p17dbecjsn45b21b9d6bda'
+    },
+    method: "GET"
+  }).then(function (response) {
 
-    for (var i =0; i<10;i++){
+
+    console.log(response)
+
+    for (var i = 0; i < 10; i++) {
       var recipeDiv = $("<div>");
       var recipeImage = $("<img>")
       var recipeTitle = $("<div>")
 
       recipeImage.attr("src", response[i].image);
-      recipeTitle.text("Title: "+ response[i].title)
+      recipeTitle.text("Title: " + response[i].title)
 
       recipeDiv.append(recipeImage, recipeTitle)
       $("#recipesection").append(recipeDiv);
+    
+      recipeIdArray.push(response[i].id);
+      recipeString = recipeIdArray.join('%2C');
 
-    console.log(recipeImage)
-    console.log(recipeTitle)
+    }
 
-      }
+    ajaxRecipeId();
+
+    function ajaxRecipeId() {
+
+      $.ajax({
+        url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk?ids=" + recipeString,
+        headers: {
+          'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+          'X-RapidAPI-Key': '98db7bca28msh8d5ab6517d13d17p17dbecjsn45b21b9d6bda'
+        },
+        method: "GET"
+      }).then(function (response) {
+
+        for (var i = 0; i < 10; i++) {
+
+          var recipeList = response[i].sourceUrl
+          console.log(recipeList)
+
+        }
+      })
+    }
   })
 };
