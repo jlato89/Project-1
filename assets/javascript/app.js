faf61223671ae9e5f64a12ea5be4e0006672a8db
Will Means <cwmeans92@gmail.com>
2019-06-19 18:13:52 -0500

    working delete button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=10&ingredients=" + ingredient + "+";
 var queryURL = "https://the-cocktail-db.p.rapidapi.com/random.php"
-var ingredient = ["eggs+", "apples+", "rice+"];
+var ingredient = [];
 var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=10&ingredients=" + ingredient + "+";
 var recipeIdArray = [];
 var recipeTitleArray = [];
@@ -88,6 +88,7 @@ $(document).ready(function () {
             ingrBtn.attr("class", "chosen-ingr-item tag is-medium")
             var ingrDelete = $('<button>')
             ingrDelete.attr("class", "delete is-small")
+            ingrDelete.attr("id", "delete-btn")
             ingrBtn.append(userSelection, ingrDelete)
             $('#chosen-ingr-list').append(ingrBtn)
             console.log(userSelection)
@@ -98,41 +99,11 @@ $(document).ready(function () {
 }) //end .ready function//
 
 
-$.ajax({
-    url: queryURL,
-    headers: {
-        "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
-        "X-RapidAPI-Key": "147749aee4mshcd867948b97e9f7p16bdacjsn257d0be55a77"
-    },
-    method: "GET"
-}).then(function (response) {
-    for (var i = 0; i < 1; i++) {
-        var drinkDisplay = response.drinks[i].strDrinkThumb
-        var drinkName = response.drinks[i].strDrink
-        var drinkInstructions= response.drinks[i].strInstructions
-        var drinkIngredient1= response.drinks[i].strIngredient1
-        var drinkIngredient2= response.drinks[i].strIngredient2
-        var drinkIngredient3= response.drinks[i].strIngredient3
-        var drinkIngredient4= response.drinks[i].strIngredient4
-        var drinkIngredient5= response.drinks[i].strIngredient5
-        var drinkIngredient6= response.drinks[i].strIngredient6
-        var drinkIngredient7= response.drinks[i].strIngredient7
-        var drinkIngredient8= response.drinks[i].strIngredient8
-        var drinkIngredient9= response.drinks[i].strIngredient9
-        console.log(response)
-        console.log(drinkDisplay)
-        console.log(drinkName)
-        console.log(drinkInstructions)
-        console.log(drinkIngredient1)
-        console.log(drinkIngredient2)
-        console.log(drinkIngredient3)
-        console.log(drinkIngredient4)
-        console.log(drinkIngredient5)
-        
-        }
-    }
-)
+$("#chosen-ingr-list").on("click", ".delete", function(event){
+    event.preventDefault()
+        $(this).parent().remove();
 
+})
 
 
 function showRecipe() {
@@ -165,3 +136,4 @@ function showRecipe() {
     }
 
 }
+
